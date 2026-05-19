d45d78d952dc3a4886b6c90b0b93751b49760f14
ilkoninn <recebovilkin256@gmail.com>
2026-05-19 21:43:03 +0400

    fix: rename aspnet identity tables to snake_case

--- services/identity-service/src/Identity.Infrastructure/Persistence/IdentityDbContext.cs
+++ services/identity-service/src/Identity.Infrastructure/Persistence/IdentityDbContext.cs
@@ -9,6 +9,14 @@ public class AppDbContext : IdentityDbContext<AppUser, IdentityRole<Guid>, Guid>
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+
+        builder.Entity<IdentityRole<Guid>>().ToTable("roles");
+        builder.Entity<IdentityUserRole<Guid>>().ToTable("user_roles");
+        builder.Entity<IdentityUserClaim<Guid>>().ToTable("user_claims");
+        builder.Entity<IdentityUserLogin<Guid>>().ToTable("user_logins");
+        builder.Entity<IdentityRoleClaim<Guid>>().ToTable("role_claims");
+        builder.Entity<IdentityUserToken<Guid>>().ToTable("user_tokens");
+
         builder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
     }
 }
