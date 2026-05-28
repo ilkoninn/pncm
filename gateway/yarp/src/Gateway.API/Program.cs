b664f32fa911b1ea966c01cf5e5a20ad85af87de
ilkoninn <recebovilkin256@gmail.com>
2026-05-28 13:54:21 +0400

    feat(cors): add CORS configuration to allow requests from specified origins

--- gateway/yarp/src/Gateway.API/Program.cs
+++ gateway/yarp/src/Gateway.API/Program.cs
@@ -3,7 +3,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000", "http://pncm.local")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapReverseProxy();
 
